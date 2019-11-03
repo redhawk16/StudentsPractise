6983228315700474ab7116d70e4a39ae4118ac45
redhawk16 <thomasgameone@gmail.com>
2019-11-03 13:01:26 +0600

    Create Project

---
+++ StudentsPract/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using StudentsPract.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentsPract
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private bool hamburger_menu_open = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            TopMenu.SelectedIndex = 0; // Set home screen page is activated after initialize components
+        }
+
+        private void hamburger_Click(object sender, RoutedEventArgs e)
+        {
+            if(hamburger_menu_open == true) {
+                hamburger_menu.Width = new GridLength(59);
+            } else {
+                hamburger_menu.Width = new GridLength(1, GridUnitType.Star);
+            }
+            hamburger_menu_open = !hamburger_menu_open;
+        }
+
+        // TopMenu hide selection
+        private void BottomListView_Selected(object sender, RoutedEventArgs e)
+        {
+            //((ListView)sender).Name
+            if (TopMenu.SelectedIndex != -1 || !TopMenu.SelectedIndex.Equals("")) {
+                TopMenu.SelectedIndex = -1;
+            }
+        }
+
+        // BottomMenu hide selection
+        private void TopListView_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            int index = TopMenu.SelectedIndex;
+
+            if (ContentView.IsInitialized)
+            {
+                switch (index)
+                {
+                    case 0: // Главное меню
+                        ContentView.Children.Clear();
+                        ContentView.Children.Add(new Home());
+                        break;
+                    case 1: //
+                        ContentView.Children.Clear();
+                        break;
+                    case 2: //
+                        ContentView.Children.Clear();
+                        break;
+                    case 3: //
+                        ContentView.Children.Clear();
+                        break;
+                    case 4: // 
+                        ContentView.Children.Clear();
+                        break;
+                    case 5: // Уведомления
+                        ContentView.Children.Clear();
+                        break;
+                    case 6: // Параметры
+                        ContentView.Children.Clear();
+                        break;
+                    default: break;
+                }
+            }
+
+            if (BottomMenu.SelectedIndex != -1 || !BottomMenu.SelectedIndex.Equals("")) { 
+                BottomMenu.SelectedIndex = -1;
+            }
+        }
+    }
+}
---
+++ StudentsPract/Pages/Home.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentsPract.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Home.xaml
+    /// </summary>
+    public partial class Home : UserControl
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+/*            AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+            WindowsPrincipal principal = (WindowsPrincipal)Thread.CurrentPrincipal;
+            WindowsIdentity identity = (WindowsIdentity)principal.Identity;*/
+
+            TextBlock_Username.Text = "Вы вошли под именем: " + Environment.UserName;
+        }
+    }
+}
